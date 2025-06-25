94fd6ea628915bc29c624f7a64558e926504fb73
Sameer <sameersk2080@gmail.com>
2025-06-25 23:40:20 +0530

    Added Download History

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import os
 import platform
 from tkinter import Tk, filedialog
+from tinydb import TinyDB, Query
+import json
+    
 
 def search(query):
     # searching and getting list of mods
@@ -20,15 +23,12 @@ def select_mod(slug,loader,version):
     
     # details of selected mod by loader and version
     mod=[items for items in data if (loader in items["loaders"] and version in items["game_versions"] and items["version_type"]=="release")]
-    return mod
+    return mod[0]
 
-def download_mod(mod):
+def download_mod(mod,folder):
     # got download link
-    dl=mod[0]["files"][0]["url"]
-    filename=mod[0]["files"][0]["filename"]
-
-    # Choose the folder to save in
-    folder = "d:\\Stuff\\Random Projects Stuff\\[Python] MC modpack Installer\\Downloaded"
+    dl=mod["files"][0]["url"]
+    filename=mod["files"][0]["filename"]
     save_path = os.path.join(folder, filename)
 
     # Downloading the file
@@ -46,7 +46,7 @@ def download_mod(mod):
 def get_mod_folder():
     path = os.path.expanduser("~")
     if platform.system() == "Windows":
-        path = os.path.join(path, "AppData", "Roaming", ".minecrafter")
+        path = os.path.join(path, "AppData", "Roaming", ".minecraft")
     elif platform.system() == "Darwin":  
         path = os.path.join(path, "Library", "Application Support", "minecraft")
     else: 
@@ -61,6 +61,14 @@ def get_mod_folder():
         root.destroy()
         return folder_selected if folder_selected else get_mod_folder()
         
+def modman(mc_folder):
+    os.chdir(mc_folder)
+    try:
+        os.mkdir(".\\modman")
+    except FileExistsError:
+        return
+        
+
 
 if __name__=="__main__":
     
@@ -77,17 +85,43 @@ if __name__=="__main__":
     #     print("Versions: ")
     #     for version in mod["versions"]:
     #         print(version+",",end=" ")
-    #     print("Slug(for selection):",mod["slug"],sep=" ")
+    #     print("\nSlug(for selection):",mod["slug"],sep=" ")
     #     print("\n")
     #     print()
     # mod_slug=str(input("Enter slug of mod to download(case-sensitive): "))
     # mod_loader=str(input("Enter loader(case-sensitive): "))
     # mod_version=str(input("Enter version(case-sensitive): "))
     # mod=select_mod(mod_slug,mod_loader,mod_version)
-    # download_mod(mod)
+    # print(json.dumps(mod[0], indent=4))
+    # # download_mod(mod)
     
     # PHASE 1.2
-    mc_folder = get_mod_folder()
-    print(mc_folder)
+    
+    #mod man and mods
+    mc_folder=get_mod_folder()
+    modman(mc_folder)
+    mods_folder=os.path.join(mc_folder,"mods")
+    modman_folder=os.path.join(mc_folder,"modman")
+    
+    # ......
+    # steps of phase 1
+    mod=select_mod("sodium","fabric","1.21.1")
+    download_mod(mod,mods_folder)
+    mod["active"]=True
+    # saving the download history in json file
+    while(True):
+        if os.path.exists(f"{mods_folder}\\{mod["files"][0]["filename"]}"):
+            with TinyDB(os.path.join(modman_folder,"mods.json")) as mods_json:
+                mods_json.insert(mod)
+                mods_json.close()
+            break
+        else:
+            download_mod(mod,mods_folder)
         
+        
+        
+    
+        
+
+    
         
