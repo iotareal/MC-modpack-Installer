e7aa1223a6415a0f9dae6bb45d1d858893c7b1ba
Sameer <sameersk2080@gmail.com>
2025-09-28 00:51:08 +0530

    Implemented One-click update feature

--- core/api/apiService.py
+++ core/api/apiService.py
@@ -4,29 +4,16 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.
 import requests
 import json
 import os
-
+from core.directory import paths
 
 # GETTERS
 # search by selected pack
 
-def getModsBulk(modids):
-    url = url = "https://api.modrinth.com/v2/projects"
-    params = {
-         'ids': json.dumps(modids)
-    }
-    
-    try:
-        response = requests.get(url,params=params)
-        response.raise_for_status()
-        return response.json()["hits"]
-    except requests.exceptions.RequestException as e:
-        print(f"[api] an error occured: {e}")
-        return []
 
 def searchProjects(query:str,selected_pack)-> list[dict]:
     loader = selected_pack.loader
     version = selected_pack.version
-    return __searchMods(query,version,loader)+__searchShaders(query,version,loader)+__searchResourcepacks(query,version,loader)
+    return __searchMods(query,version,loader)+__searchShaders(query,version,loader)+__searchResourcepacks(query,version)
 
 def __searchMods(query:str,version:str,loader:str) -> list[dict]:
     url = "https://api.modrinth.com/v2/search"
@@ -109,6 +96,95 @@ def __searchResourcepacks(query:str,version:str) -> list[dict]:
         print(f"[api] an error occured: {e}")
         return []
     
+def get_mod_version(mod,selected_pack):
+    if mod['id']:
+        url = f"https://api.modrinth.com/v2/project/{mod['id']}/version"
+    else:
+        url = f"https://api.modrinth.com/v2/project/{mod['project_id']}/version"
+        
+    
+    if mod['project_type'] not in ['shader','resourcepack']:
+        params = {
+            "game_versions" : json.dumps([selected_pack.version]),
+            "loaders" : json.dumps([selected_pack.loader]),
+        }
+    else:
+        params = {
+            "game_versions" : json.dumps([selected_pack.version])
+        }
+    try:
+        response = requests.get(url,params=params)
+        response.raise_for_status()
+        return response.json()
+    except Exception as e:
+        print(e)
+        return []
+
+def getModsBulk(modids):
+    url = "https://api.modrinth.com/v2/projects"
+    params = {
+         'ids': json.dumps(modids)
+    }
+    
+    try:
+        response = requests.get(url,params=params)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"[api] an error occured: {e}")
+        return []
+
+def downloadMod(mod):
+    file = mod['files'][0]
+    # Create the full path for the file.
+    if mod['project_type'] == 'shader':
+        save_path = os.path.join(paths.get_shaderpacks_dir(), file['filename'])
+    elif mod['project_type'] == 'resourcepack':
+        save_path = os.path.join(paths.get_rpacks_dir(), file['filename'])
+    elif mod['project_type'] == 'mod':
+        save_path = os.path.join(paths.get_mods_dir(), file['filename'])
+    else:
+        save_path = os.path.join(paths.get_buffer_dir(), file['filename'])
+
+    if os.path.exists(save_path):
+        print("[api-service] mod already exists")
+        return
+    # Stream the download.
+    response = requests.get(file['url'], stream=True)
+    response.raise_for_status()  # Raises an error for bad responses.
+
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:  # filter out keep-alive chunks
+                file.write(chunk)
+
+    print(f"[api-service] downloaded file saved to {save_path}")
+
+def downloadModLatest(mod):
+    file = mod[0]['files'][0]
+    # Create the full path for the file.
+    if mod['project_type'] == 'shader':
+        save_path = os.path.join(paths.get_shaderpacks_dir(), file['filename'])
+    elif mod['project_type'] == 'resourcepack':
+        save_path = os.path.join(paths.get_rpacks_dir(), file['filename'])
+    elif mod['project_type'] == 'mod':
+        save_path = os.path.join(paths.get_mods_dir(), file['filename'])
+    else:
+        save_path = os.path.join(paths.get_buffer_dir(), file['filename'])
+
+    if os.path.exists(save_path):
+        print("[api-service] mod already exists")
+        return
+    # Stream the download.
+    response = requests.get(file['url'], stream=True)
+    response.raise_for_status()  # Raises an error for bad responses.
+
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            if chunk:  # filter out keep-alive chunks
+                file.write(chunk)
+
+    print(f"[api-service] downloaded file saved to {save_path}")
 # DEPRECATED
 
 # # seach by query and version NO USAGE
@@ -147,19 +223,6 @@ def __searchResourcepacks(query:str,version:str) -> list[dict]:
 #         print(e)
 #         return []
 
-# def get_mod_version(id:str,selected_pack):
-#     url = f"https://api.modrinth.com/v2/project/{id}/version"
-#     params = {
-#         "game_versions" : json.dumps([selected_pack["minecraft_version"]]),
-#         "loaders" : json.dumps([selected_pack["loader"]])
-#     }
-#     try:
-#         response = requests.get(url,params=params)
-#         response.raise_for_status()
-#         return response.json()
-#     except Exception as e:
-#         print(e)
-#         return []
     
 # def get_bulk_mods(project_ids:list[str]) -> list[dict]:
 #     url = "https://api.modrinth.com/v2/projects"
--- core/services/modpackService.py
+++ core/services/modpackService.py
@@ -6,14 +6,17 @@ from core.classes.ModPack import ModPack
 from core.api import apiService as api
 from core.api import versionService as vers
 from core.exceptions import ModPackExceptions as mperr
+from core.directory import paths 
+from tabulate import tabulate
+from colorama import Fore,Style,init
 import shutil 
 import pickle
 
+init(autoreset=True)
 SELECTED_PACK = None
 SELECTED_PACK_FILE = ".\\selectedpack.pkl"
 MODPACKS = ".\\packs.pkl"
 
-my_modpack = ModPack(name="My Modpack", version="1.21.1", loader="fabric")
 
 def write(pack):
     try:
@@ -50,10 +53,27 @@ def setLastSelectedPack():
     with open(SELECTED_PACK_FILE,'wb') as file:
         return pickle.dump(SELECTED_PACK,file)
 
+def getActivepack():
+    packs = getPacks()
+    for pack in packs:
+        if pack.active:
+            return pack
+
+def updateAllPacks():
+    global SELECTED_PACK
+    setLastSelectedPack()
+    packs = getPacks()
+    for i,pack in enumerate(packs):
+        if pack.id == SELECTED_PACK.id:
+            break
+    packs[i] = SELECTED_PACK
+    writeall(packs)
+    print(f"{Fore.GREEN}[modpackService] updated all packs")
+    
 def createModpack(name:str, version:str, loader:str):
     pack = ModPack(name=name, version=version, loader=loader)
     write(pack)
-    return f"[modpackService] create modpack id: {pack.id}"
+    print(f"{Fore.GREEN}[modpackService] create modpack id: {pack.id}")
 
 def changeActive(id,active=True):
     packs = getPacks()
@@ -64,7 +84,8 @@ def changeActive(id,active=True):
         if(pack.id == id): 
             pack.active = active
             writeall(packs)
-            return f"[modpackService] Modpack: {id} active:{active}"
+            print(f"{Fore.GREEN}[modpackService] Modpack: {id} active:{active}")
+            return
     raise mperr.ModPackNotExistsError(id)
 
 def selectModPack(id):
@@ -75,33 +96,174 @@ def selectModPack(id):
             SELECTED_PACK = pack
             changeActive(SELECTED_PACK.id)
             setLastSelectedPack()            
-            return f"[modpackService] Modpack: {SELECTED_PACK.id} is now selected"
+            print(f"{Fore.GREEN}[modpackService] Modpack: {SELECTED_PACK.id} is now selected")
     
     raise mperr.ModPackNotExistsError(id)
 
-def addModtoSelectedPack():
+def addModtoSelectedPack(query):
     global SELECTED_PACK
-    query = input("Enter Modname: ")
-    foundmods = api.search_mods(query,SELECTED_PACK)
+    foundmods = api.searchProjects(query,SELECTED_PACK)
+    headers = ["Project_ID", "Title", "Project_Type"]
+    tabledata = []
     for mod in foundmods:
-        print(f"{mod['project_id']}\t{mod['title']}\t{mod['project_type']}")
-            
+        tabledata.append([
+            mod.get('project_id', 'N/A'), 
+            mod.get('title', 'N/A'), 
+            mod.get('project_type', 'N/A')
+        ])
+    print(tabulate(tabledata,headers=headers, tablefmt="rounded_grid",showindex="always"))
+    while(True):
+        try:
+            choice = int(input("\nPlease Enter the Index No: "))
+            if(choice < 0 or choice >=len(tabledata)):
+                print(f"Only acceptable values are from {0} to {len(tabledata)-1}")
+                continue
+            else:
+                break
+        except Exception:
+            print(f"Only acceptable values are from {0} to {len(tabledata)-1}")
+            continue
+    selected_mod = foundmods[choice]
+    print(f"You've Selected {selected_mod["title"]} which is a {selected_mod["project_type"]}")
+    
+    ptype = selected_mod["project_type"]
+    # GETTING VERSION
+    foundmods = api.get_mod_version(selected_mod,SELECTED_PACK)
+    tabledata.clear()
+    headers = ["version ID","version number"]
+    for mod in foundmods:
+        tabledata.append([
+            mod.get('id','N/A'),
+            mod.get('version_number','N/A')
+        ])
+    print(tabulate(tabledata,headers=headers, tablefmt="rounded_grid",showindex="always"))
+    while(True):
+        try:
+            choice = int(input("\nPlease Enter the Index No: "))
+            if(choice < 0 or choice >=len(tabledata)):
+                print(f"{Fore.RED}Only acceptable values are from {0} to {len(tabledata)-1}")
+                continue
+            else:
+                break
+        except Exception:
+            print(f"{Fore.RED}Only acceptable values are from {0} to {len(tabledata)-1}")
+            continue
+    selected_mod = foundmods[choice]
+    selected_mod['project_type'] = ptype
+    SELECTED_PACK.mods.append(selected_mod)
+    updateAllPacks()
+    api.download_mod(selected_mod)
+
+def removeModfromSelectedPack():
+    if len(SELECTED_PACK.mods) == 0:
+        print(f"{Fore.GREEN}[modpackService] {SELECTED_PACK.name} has no mods")
+        return 
+    mods = []
+    for mod in SELECTED_PACK.mods:
+        mods.append(mod['project_id'])
+    mods = api.getModsBulk(mods)
+    headers = ["Mod title","Mod type"]
+    tabledata = []
+    for mod in mods:
+        tabledata.append([
+            mod.get("title","N/A"),
+            mod.get('project_type',"N/A")
+        ])
+    print(tabulate(tabledata,headers=headers, tablefmt="rounded_grid",showindex="always"))
+    while(True):
+        try:
+            choice = int(input("\nPlease Enter the Index No: "))
+            if(choice < 0 or choice >=len(tabledata)):
+                print(f"{Fore.RED}Only acceptable values are from {0} to {len(tabledata)-1}")
+                continue
+            else:
+                break
+        except Exception:
+            print(f"{Fore.RED}Only acceptable values are from {0} to {len(tabledata)-1}")
+            continue
+    for i,mod in enumerate(SELECTED_PACK.mods):
+        if mod == mods[choice]: break
+    if SELECTED_PACK.mods[i]['project_type'] == 'mod':
+        src = os.path.join(paths.get_mods_dir(),SELECTED_PACK.mods[i]['files'][0]['filename'])
+    elif SELECTED_PACK.mods[i]['project_type'] == 'resourcepack':
+        src = os.path.join(paths.get_rpacks_dir(),SELECTED_PACK.mods[i]['files'][0]['filename'])
+    elif SELECTED_PACK.mods[i]['project_type'] == 'shader':
+        src = os.path.join(paths.get_shaderpacks_dir(),SELECTED_PACK.mods[i]['files'][0]['filename'])
+    
+    if not os.path.exists(src):
+        SELECTED_PACK.mods.pop(i)
+        updateAllPacks()
+        return None
+    
+    dest = paths.get_buffer_dir()
+    shutil.move(src,dest)
+    SELECTED_PACK.mods.pop(i)
+    updateAllPacks()
+    
 # MAIN COURSE
-# def updateModPack(id,version):
-#     global SELECTED_PACK
-#     if SELECTED_PACK.version == version: return f"[modpackService] version is already {version}"
-#     print(f"Updating Modpack: {SELECTED_PACK.name} please do not exit.. ")
-#     SELECTED_PACK.version = version
-#     print(f"[modpackService] changed version to {SELECTED_PACK.version}")
-#     mods = SELECTED_PACK.mods
-#     modids = tuple(s.split("-")[0] for s in mods)
+def clear_folder(folder_path):
+    if os.path.exists(folder_path):
+        shutil.rmtree(folder_path)
+    os.makedirs(folder_path)
+    print(f"{Fore.GREEN} removed all contents of {folder_path}")
+    
+def updateModPack(version):
+    global SELECTED_PACK
+    print(f"\n{Fore.YELLOW} Removing existing mods")
+    clear_folder(paths.get_mods_dir())
+    clear_folder(paths.get_rpacks_dir())
+    clear_folder(paths.get_shaderpacks_dir())
+    print(f"\n{Fore.GREEN} All mods removed successfully")
+    
+    print(f"\n{Fore.YELLOW} Attempting to update pack {SELECTED_PACK.name}")
+    SELECTED_PACK.version = version
+    mods = []
+    for mod in SELECTED_PACK.mods:
+        mods.append(mod["project_id"])
+    
+    mods = api.getModsBulk(mods)
+    
+    newmods = []
+    for mod in mods:
+        newmod = api.get_mod_version(mod,SELECTED_PACK)
+        if newmod:
+            newmod = newmod[0]
+            newmod['project_type'] = mod['project_type']
+            newmods.append(newmod)
+        else:
+            print(f"{Fore.RED}[modpackService] failed to update {mod['title']}")
+            newmods.append(mod)
+        
+    for mod in newmods:
+        api.downloadMod(mod)
+    SELECTED_PACK.mods = newmods
+    updateAllPacks()
+    print(f"{Fore.GREEN} updated pack {SELECTED_PACK.name}")
+    
+    
 
 # TEST
 # def moveFilestoBuffer():
-print(selectModPack("0b55be5d-c878-4d1b-a167-bba0e930dfd5"))
+# selectModPack("0b55be5d-c878-4d1b-a167-bba0e930dfd5")
 # packs = getPacks()
-# print(getLastSelectedPack())
-addModtoSelectedPack()
 # for pack in packs:
-#     print(pack)
+#     print(pack.name)
+#     print(pack.version)
+#     for mod in pack.mods:
+#         print(mod['project_id'],mod['version_number'],sep=" --- ")
+#     print("\n")
+    
+    
+SELECTED_PACK = getLastSelectedPack()
+# updateAllPacks()
+# print(getLastSelectedPack())
+# # SELECTED_PACK.mods.append("lol")
+# # setLastSelectedPack()
+# print(getLastSelectedPack())
+# # print(SELECTED_PACK)
+# query = input("Enter query: ")
+# addModtoSelectedPack(query)
+# print(getLastSelectedPack())
 
+
+# removeModfromSelectedPack()
